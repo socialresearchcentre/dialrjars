08cdec77d73213c27ae6ebcca4c6f3600837d3a9
Danny Smith <dsmith@srcentre.com.au>
2019-03-12 13:22:26 +1100

    Initial commit

---
+++ R/dialrjars.R
@@ -0,0 +1,7 @@
+#' Package required libphonenumber jars for the dialr package
+#'
+#' The dialrjars package collects libphonenumber jars required for the dialr package.
+#'
+#' @import rJava
+#' @keywords internal
+"_PACKAGE"
