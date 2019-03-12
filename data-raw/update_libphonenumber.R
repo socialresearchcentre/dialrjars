3bc21daf1bdbfa5e8f501f4c46ff5a7897aa0de5
Danny Smith <dsmith@srcentre.com.au>
2019-03-12 15:59:03 +1100

    Extend auto update code
    AUto-update code now deals with git commits and provides better messaging:
    * Download latest libphonenumber
    * Update version in description
    * If any updates, commit changes
    * If any updates, build binary

--- data-raw/update_libphonenumber.R
+++ data-raw/update_libphonenumber.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(stringr)
 
 update_libphonenumber <- function(pkg_location = ".") {
-  message("dialr: checking for latest version of libphonenumber")
+  message("dialrjars: checking for latest version of libphonenumber")
   jar_file <- list.files(file.path(pkg_location, "inst/java"), ".*.jar$")
 
   current <- sub("^libphonenumber-(.*).jar$", "\\1", jar_file)
@@ -15,7 +15,7 @@ update_libphonenumber <- function(pkg_location = ".") {
 
   tryCatch({
     if (current != latest) {
-      message("dialr: updating libphonenumber from version ", current, " to ", latest)
+      message("dialrjars: updating libphonenumber from version ", current, " to ", latest)
       download.file(paste0("http://repo1.maven.org/maven2/com/googlecode/libphonenumber/libphonenumber/",
                            latest, "/libphonenumber-", latest, ".jar"),
                     paste0("inst/java/libphonenumber-", latest, ".jar"),
@@ -23,9 +23,9 @@ update_libphonenumber <- function(pkg_location = ".") {
 
       file.remove(file.path(pkg_location, "inst/java", jar_file))
     }
-    message("dialr: up to date!")
+    message("dialrjars: libphonenumber up to date!")
   },
-  error = function(e) { message("dialr: libphonenumber update failed, continuing with version ", current) })
+  error = function(e) { message("dialrjars: libphonenumber update failed, continuing with version ", current) })
 
   if (str_detect(latest, "[0-9]+\\.[0-9]+\\.[0-9]+"))
     latest
@@ -34,14 +34,42 @@ update_libphonenumber <- function(pkg_location = ".") {
 
 }
 
+get_pkg_version <- function(pkg_location = ".") {
+  desc <- readLines(file.path(pkg_location, "DESCRIPTION"))
+  str_replace(desc[str_detect(desc, "^Version:")], "^Version:\\s*", "")
+}
+
 update_pkg_version <- function(vers, pkg_location = "."){
   desc <- readLines(file.path(pkg_location, "DESCRIPTION"))
   desc[str_detect(desc, "^Version:")] <- paste0("Version: ", vers)
   writeLines(desc, file.path(pkg_location, "DESCRIPTION"))
 
-  return(vers)
+  invisible(vers)
 }
 
-vers <- update_libphonenumber()
-update_pkg_version(vers)
-devtools::build(binary = TRUE, args = c('--preclean'))
+update_dialrjars <- function() {
+  current <- get_pkg_version()
+
+  message("dialrjars: checking for package updates for version ", current)
+  latest <- update_libphonenumber()
+
+  if (current == latest) {
+    message("dialrjars: no changes required, exiting...")
+    return(invisible(latest))
+  }
+
+  message("dialrjars: updating version in DESCRIPTION to ", latest)
+  update_pkg_version(latest)
+
+  message("dialrjars: committing changes to git")
+  git2r::add(path = "inst/java")
+  git2r::add(path = "DESCRIPTION")
+  git2r::commit(message = paste0("Update libphonenumber to version ", latest))
+
+  message("dialrjars: building new binary")
+  devtools::build(binary = TRUE, args = c('--preclean'))
+
+  invisible(latest)
+}
+
+update_dialrjars()
